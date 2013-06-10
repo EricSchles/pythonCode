690de560ba8d69c5efe02e79fee1f56bca639540
Eric Schles <ericschles@gmail.com>
2013-06-10 13:35:13 -0400

    game updated

--- ex45.py
+++ ex45.py
@@ -1,5 +1,5 @@
 from ex45Room import *
-from ex45db import *
+#from ex45db import *
 
 
 class Map(object):
@@ -23,4 +23,23 @@ class Map(object):
         return self.next(self.start)
 
 
+#Iterator class:
+#iterates through the map in sequential order
+#this essential looks like a linked list that can only be moved
+#through forward...
+class Engine(object):
+    def __init__(self, room_map):
+        self.room_map = room_map
 
+    def play(self):
+        current_room = self.room_map.opening()
+        
+        while True:
+            print "\n --------"
+            next_room = current_room.enter()
+            current_room = self.room_map.next(next_room)
+
+
+a_map = Map('intro')
+a_game = Engine(a_map)
+a_game.play()
